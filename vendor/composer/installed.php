83cacafe84994c712aace7950005b3abac86840d
OlivierMongeot <olivier.mongeot@gmail.com>
2021-10-08 12:44:50 +0200

    postgre

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-postgre',
+        'version' => 'dev-postgre',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'eb25b9defa055e12dbcafe52cc1119b0e6c7d5ba',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-postgre',
+            'version' => 'dev-postgre',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'eb25b9defa055e12dbcafe52cc1119b0e6c7d5ba',
+            'dev_requirement' => false,
+        ),
+    ),
+);
